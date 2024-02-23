bdb8f0e47cdf68a04fc27239d2b953c04a196106
Violet-Riah <violettanjombwa@gmail.com>
2024-02-23 09:43:40 +0300

    create files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(){
+  println("Hello Beverly")
+    modulus(7,2)
+    sum(4,5,7,8)
+    println("Crocheting")
+}
+fun printhello(name: String){
+    println(name)
+}
+fun modulus(num1:Int,num2:Int,):Int{
+    var result = num1 * num2
+    return result
+}
+fun sum(num1:Int,num2:Int,num3:Int,num4:Int,):Int{
+    var result = num1 + num2 + num3 + num4
+    return result
+}
+fun funnyFact(word : String){
+    println(word)
+}
+
+p
